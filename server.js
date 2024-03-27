b0bd178daaf710297eb33d7b0c3e6b21e2165cc4
gagandeepsinghbrar <gaganbrarca@gmail.com>
2024-03-26 20:11:37 -0400

    fix cors.

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const { Pool } = require('pg');
 
 const app = express();
+
+app.use(cors());
+
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 // Middleware to parse JSON bodies
